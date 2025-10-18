d69bbf4e322233ef9ec466af5029054db287d43d
Daniel Dao <danieldkdao@gmail.com>
2025-10-18 10:43:10 -0500

    finished the app

--- mobile/utils/api.ts
+++ mobile/utils/api.ts
@@ -2,6 +2,7 @@ import { useAuth } from "@clerk/clerk-expo";
 import axios from "axios";
 import { useMemo } from "react";
 
+// const baseURL = "http://10.0.2.2:5000/api";
 const baseURL = process.env.EXPO_PUBLIC_BACKEND_URL;
 
 const useApi = () => {
